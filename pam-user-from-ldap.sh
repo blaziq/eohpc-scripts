8f3717a4cb5e2e3ad45cbe63a51e0ab864b24428
Blazej Fitrzyk <blazej.fitrzyk@solenix.ch>
2026-03-05 22:09:30 +0100

    .

--- pam-user-from-ldap.sh
+++ pam-user-from-ldap.sh
@@ -68,7 +68,8 @@ set_quotas() {
     # GPU quota
     [[ ! ${GPU_QUOTA} =~ ^[0-9]+ ]] && GPU_QUOTA=""
     GPU_DEVICES=$(/usr/local/sbin/pam-user-gpu-devices.py "${GPU_QUOTA}")
-    
+    logger -p authpriv.notice "${0} set_quotas:${GPU_QUOTA}"
+
     # This sets the slice quota in runtime so it is not permanent but active until next login or reboot
     # There'll be no setting in /etc/systemd/system.control/${SLICE}
     # but only in /run/systemd/system.control/${SLICE}.d
