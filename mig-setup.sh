7c17a91e049a9dd1a99414ca4c45ab248639e0ba
Blazej Fitrzyk <blazej.fitrzyk@solenix.ch>
2026-03-06 17:41:48 +0100

    mig-setup

---
+++ mig-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+MIG_CONFIG="/usr/local/etc/mig-setup.conf"
+MIG_SLICES=$(tr "\n" " " 2>/dev/null <${MIG_CONFIG})
+
+if [[ "${MIG_SLICES}" == "" ]]; then
+    logger -t mig-setup "GPU layout not found in ${MIG_CONFIG}, skipping GPU mig configuration"
+    exit 0
+fi
+
+if [[ ! ${MIG_SLICES} =~ ^[0-9]+g\.[0-9]+gb(,[0-9]+g\.[0-9]+gb)*$ ]]; then
+    logger -t mig-setup "GPU layout invalid value '${MIG_SLICES}' in ${MIG_CONFIG}"
+    exit 1
+fi
+
+MIG_SLICES_VALS=(${MIG_SLICES//,/ })
+AVAILABLE_GI_PROFILES=$(/usr/bin/nvidia-smi mig -lgip | grep -o "[0-9]\+g\.[0-9]\+gb[^[:space:]]*")
+for S in ${MIG_SLICES_VALS[@]}; do
+    if ! grep -qo "${S}" <<< "${AVAILABLE_GI_PROFILES}"; then
+        logger -t mig-setup "GPU layout invalid slice '${S}'"
+        exit 1
+    fi
+done
+
+GPU=0
+
+# Enable MIG (idempotent)
+nvidia-smi -i $GPU -mig 1 || true
+
+# Remove existing instances (if any)
+nvidia-smi mig -dgi -i $GPU || true
+nvidia-smi mig -dci -i $GPU || true
+
+# Create two 20GB instances
+nvidia-smi mig -cgi ${MIG_SLICES} -i $GPU
+nvidia-smi mig -cci -i $GPU
--- setup-ldap-quotas.sh
+++ setup-ldap-quotas.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 BIN_DIR="/usr/local/sbin"
+ETC_DIR="/usr/local/etc"
 PAM_DIR="/etc/pam.d"
+SVC_DIR="/etc/systemd/system"
 
 BASE="pam-user-from-ldap"
 CHECK_ACCESS="pam-user-check-access"
@@ -15,7 +17,9 @@ GPU_DEVICES="pam-user-gpu-devices.py"
 SCRIPT_BASE="${BIN_DIR}/${BASE}.sh"
 SCRIPT_CHECK_ACCESS="${BIN_DIR}/${CHECK_ACCESS}.sh"
 SCRIPT_SET_QUOTAS="${BIN_DIR}/${SET_QUOTAS}.sh"
-SCRIPT_GPU_DEVICES="${BIN_DIR}/${GPU_DEVICES}.sh"
+SCRIPT_GPU_DEVICES="${BIN_DIR}/${GPU_DEVICES}"
+
+echo "Installing scripts for checking user access based on groups from LDAP and setting resource quotas in ${BIN_DIR}..."
 
 install -m 711 "${BASE}.sh" "${BIN_DIR}"
 install -m 711 "${GPU_DEVICES}" "${BIN_DIR}"
@@ -23,18 +27,41 @@ ln -sfr "${SCRIPT_BASE}" "${SCRIPT_CHECK_ACCESS}"
 ln -sfr "${SCRIPT_BASE}" "${SCRIPT_SET_QUOTAS}"
 
 # -----------------------------------------------------------------------------
-# 2. Set up LDAP communication over the internal IP
+# 2. Install script for setting up GPU mig slicing on boot
+# -----------------------------------------------------------------------------
+MIG_SETUP="mig-setup"
+SCRIPT_MIG_SETUP="${MIG_SETUP}.sh"
+CONFIG_MIG_SETUP="${MIG_SETUP}.conf"
+SERVICE_MIG_SETUP="${MIG_SETUP}.service"
+
+echo "Installing script for setting up GPU mig slicing on boot ${BIN_DIR}..."
+
+install -m 711 "${SCRIPT_MIG_SETUP}.sh" "${BIN_DIR}"
+touch "${ETC_DIR}/${CONFIG_MIG_SETUP}"
+install -m 644 "${SERVICE_MIG_SETUP}" "${SVC_DIR}"
+systemctl daemon-reload
+systemctl enable ${MIG_SETUP}
+systemctl start ${MIG_SETUP}
+
+# -----------------------------------------------------------------------------
+# 3. Set up LDAP communication over the internal IP
 # -----------------------------------------------------------------------------
 LDAP_CONF="/etc/ldap.conf"
 LDAP_IP="10.0.0.30"
 LDAP_URI="ldap://${LDAP_IP}/"
+
+echo "Setting up LDAP communication over the internal IP in ${LDAP_CONF}..."
+
 sed -i "s/^[[:space:]]*uri[[:space:]]\+\(.*\)$/uri ${LDAP_URI//\//\\/}/" "${LDAP_CONF}"
 
 # -----------------------------------------------------------------------------
-# 3. Allow logind to communicate with the LDAP server
+# 4. Allow logind to communicate with the LDAP server
 # -----------------------------------------------------------------------------
 LOGIND_OVERRIDE_CONF_DIR="/etc/systemd/system/systemd-logind.service.d"
 LOGIND_OVERRIDE_CONF="${LOGIND_OVERRIDE_CONF_DIR}/override.conf"
+
+echo "Setting up logind for communication with the LDAP server in ${LOGIND_OVERRIDE_CONF}..."
+
 # Fix logind configuration to allow configuration with LDAP
 mkdir -p "${LOGIND_OVERRIDE_CONF_DIR}"
 cat << 'EOF' > "${LOGIND_OVERRIDE_CONF}"
@@ -46,20 +73,24 @@ systemctl daemon-reload
 systemctl restart systemd-logind
 
 # -----------------------------------------------------------------------------
-# 4. Let all users from group 'philab' through in /etc/security/access.conf
+# 5. Let all users from group 'philab' through in /etc/security/access.conf
 #    They will be later allowed or denied by checking groups in the script
 # -----------------------------------------------------------------------------
 ACCESS_CONFIG="/etc/security/access.conf"
 ACCESS_ALLOW_PHILAB="+:(philab):ALL"
 
+echo "Allowing access for 'philab' group in ${ACCESS_CONFIG}..."
+
 grep -Fqx "${ACCESS_ALLOW_PHILAB}" "${ACCESS_CONFIG}" || \
     sed -i "/^-:ALL:ALL[[:space:]]*$/i ${ACCESS_ALLOW_PHILAB}" "${ACCESS_CONFIG}"
     
 # -----------------------------------------------------------------------------
-# 5. Check access in PAM based on user groups from LDAP
+# 6. Check access in PAM based on user groups from LDAP
 # -----------------------------------------------------------------------------
 PAM_CONFIG_SSHD="${PAM_DIR}/sshd"
 PAM_INCLUDE_CHECK_ACCESS="@include ${CHECK_ACCESS}"
+
+echo "Configuring PAM to check access based on user groups from LDAP in ${PAM_CONFIG_SSHD}..."
 
 cat << EOF > "${PAM_DIR}/${CHECK_ACCESS}"
 account required pam_exec.so quiet ${SCRIPT_CHECK_ACCESS}
@@ -69,14 +100,18 @@ grep -Fqx "${PAM_INCLUDE_CHECK_ACCESS}" "${PAM_CONFIG_SSHD}" || \
     sed -i "/^@include[[:space:]]\\+common-account[[:space:]]*$/i ${PAM_INCLUDE_CHECK_ACCESS}" "${PAM_CONFIG_SSHD}"
 
 # -----------------------------------------------------------------------------
-# 6. Set up CPU and RAM quotas in PAM user session:
+# 7. Set up CPU, RAM and GPU quotas in PAM user session:
 # -----------------------------------------------------------------------------
+PAM_CONFIG_QUOTAS="${PAM_DIR}/${SET_QUOTAS}"
 PAM_CONFIG_SESSION="/etc/pam.d/common-session"
 PAM_INCLUDE_SET_QUOTAS="@include ${SET_QUOTAS}"
 
-cat << EOF > "${PAM_DIR}/${SET_QUOTAS}"
+echo "Configuring PAM to set up CPU, RAM and GPU quotas in user session based on user groups from LDAP in ${PAM_CONFIG_QUOTAS} and ${PAM_CONFIG_SESSSION}..."
+
+cat << EOF > "${PAM_CONFIG_QUOTAS}"
 session optional pam_exec.so seteuid ${SCRIPT_SET_QUOTAS}
 EOF
 
 grep -Fqx "${PAM_INCLUDE_SET_QUOTAS}" ${PAM_CONFIG_SESSION} || \
     echo "${PAM_INCLUDE_SET_QUOTAS}" >> ${PAM_CONFIG_SESSION}
+
